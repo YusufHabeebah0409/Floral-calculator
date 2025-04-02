66f1df33b709d71304c2b0c1423b77e245a77216
YusufHabeebah0409 <ojuolapehabeebah@gmail.com>
2025-04-02 12:43:33 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+function divide() {
+    var firstName = num1.value
+    var secondName = num2.value
+    var quotient = firstName / secondName
+    var answerGangan = " The Answer is " + quotient
+    console.log(answerGangan);
+    showAnswer.innerHTML = answerGangan
+}
+function addition() {
+    var firstName = Number(num1.value) 
+    var secondName =Number(num2.value) 
+    var sum = firstName + secondName
+    var answerGangan = " The Answer is " + sum
+    console.log(answerGangan);
+    showAnswer.innerHTML = answerGangan
+
+}
+function subtraction() {
+    var firstName = num1.value
+    var secondName = num2.value
+    var subtract = firstName - secondName
+    var answerGangan = " The Answer is " + subtract
+    console.log(answerGangan);
+    showAnswer.innerHTML = answerGangan
+
+}
+function multiplication() {
+    var firstName = num1.value
+    var secondName = num2.value
+    var multiple = firstName * secondName
+    var answerGangan = " The Answer is " + multiple
+    console.log(answerGangan);
+    showAnswer.innerHTML = answerGangan
+
+}
