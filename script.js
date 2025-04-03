2085f3c847878e3aedd9e1f1c6be696ed85f1189
YusufHabeebah0409 <ojuolapehabeebah@gmail.com>
2025-04-03 15:13:15 +0100

    Add input validation to arithmetic functions and create VSCode settings file

--- script.js
+++ script.js
@@ -1,40 +1,52 @@
 function addition() {
-    var firstNumber = Number(number1.value) 
-    var secondNumber = Number(number2.value) 
-    var sum = firstNumber + secondNumber
-    // console.log(sum);
-    var showResult = " The Sum is " + sum
-    showAnswer.innerHTML = showResult
-    
+    if(number1.value =="" && number2.value ==""){
+        showAnswer.innerHTML = "Input a Number"
+     } else{
+        var firstNumber = Number(number1.value) 
+        var secondNumber = Number(number2.value) 
+        var sum = firstNumber + secondNumber
+        // console.log(sum);
+        var showResult = " The Sum is " + sum
+        showAnswer.innerHTML = showResult
+    }
 }
 
 function subtraction() {
+    if(number1.value =="" && number2.value ==""){
+        showAnswer.innerHTML = "Input a Number"
+     } else{
     var firstNumber = Number(number1.value) 
     var secondNumber = Number(number2.value) 
     var subtract = firstNumber - secondNumber
     // console.log(sum);
     var showResult = " The Subtraction is " + subtract
     showAnswer.innerHTML = showResult
-    
+     }
 }
 
 function division() {
+    if(number1.value =="" && number2.value ==""){
+        showAnswer.innerHTML = "Input a Number"
+     } else{
     var firstNumber = Number(number1.value) 
     var secondNumber = Number(number2.value) 
     var divide = firstNumber / secondNumber
     // console.log(sum);
     var showResult = " The Division is " + divide
     showAnswer.innerHTML = showResult
-    
+     }
 }
 
 
 function multiplication() {
+    if(number1.value =="" && number2.value ==""){
+        showAnswer.innerHTML = "Input a Number"
+     } else{
     var firstNumber = Number(number1.value) 
     var secondNumber = Number(number2.value) 
     var multiple = firstNumber * secondNumber
     // console.log(sum);
     var showResult = " The Multiplication is " + multiple
     showAnswer.innerHTML = showResult
-    
+     }
 }
